bfa318fe8c4106e36c347790b2bbc447082b3aa0
Ori Baruch <59767980+oribaruch1995@users.noreply.github.com>
2021-06-29 17:01:46 +0300

    Added Side_Menu scene: holds a side menu with 2 Singletons 1. sidemenu for other scenes 2. audiomanager for sounds Added an AudioManager - needs updates Added SideMenu - needs update

---
+++ Assets/Scripts/SideMenu/Side_Menu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Side_Menu : MonoBehaviour
+{
+    #region Singleton
+    private static Side_Menu _instance;
+
+    public static Side_Menu Instance
+    {
+        get { return _instance; }
+    }
+    #endregion
+    [SerializeField] GameObject menuButton;
+    [SerializeField] GameObject menuCanvas;
+    
+    private void Awake()
+    {
+        #region Singleton
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+        #endregion
+        menuCanvas.SetActive(false);
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public void DisplayMenu()
+    {
+        if(menuCanvas.activeInHierarchy)
+            menuCanvas.SetActive(false);
+        else
+            menuCanvas.SetActive(true);
+        AudioManager.instance.Play("Button1");
+    }
+}
