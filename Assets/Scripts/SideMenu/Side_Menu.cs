d6bedb2a0e8878a13051cc277bb6f2b68d820b06
Ori Baruch <59767980+oribaruch1995@users.noreply.github.com>
2021-06-29 18:40:20 +0300

    Added to Sidemenu ContactUs menu contains: 1. Link to site 2. send form

--- Assets/Scripts/SideMenu/Side_Menu.cs
+++ Assets/Scripts/SideMenu/Side_Menu.cs
@@ -14,6 +14,8 @@ public class Side_Menu : MonoBehaviour
     #endregion
     [SerializeField] GameObject menuButton;
     [SerializeField] GameObject menuCanvas;
+    [SerializeField] GameObject mainButtons;
+    [SerializeField] GameObject contactMenu;
     
     private void Awake()
     {
@@ -35,12 +37,49 @@ public class Side_Menu : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Func to switch the side menu display on screen on/off
+    /// </summary>
     public void DisplayMenu()
     {
         if(menuCanvas.activeInHierarchy)
             menuCanvas.SetActive(false);
         else
+        {
             menuCanvas.SetActive(true);
+            if (!mainButtons.activeInHierarchy)
+            {
+                mainButtons.SetActive(true);
+                contactMenu.SetActive(false);
+            }
+            else
+                contactMenu.SetActive(false);         
+        }
         AudioManager.instance.Play("Button1");
+    }
+
+    /// <summary>
+    /// Control over Contact Us button in side menu, switches displays
+    /// </summary>
+    public void ContactUs()
+    {
+        if (mainButtons.activeInHierarchy)
+        {
+            mainButtons.SetActive(false);
+            contactMenu.SetActive(true);
+        }
+        else
+        {
+            contactMenu.SetActive(false);
+            menuCanvas.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// Open in a browser challange18 Site
+    /// </summary>
+    public void LinkToSite()
+    {
+        Application.OpenURL("https://ting.global/");
     }
 }
