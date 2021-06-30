cfc24de1f0b11fac53c8770c136dea49fe7c6751
iddo0010 <iddo0010@gmail.com>
2021-06-30 15:48:32 +0300

    Created BME namespace with JsonManager, Visuals(showtoggle) and Connections(signout) fixed Extract Data in JsonManager added another PushData Method in JsonManager for pushing changes in an existing account

--- Assets/Scripts/ChallengeManagment/CreateChallenge.cs
+++ Assets/Scripts/ChallengeManagment/CreateChallenge.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BME;
 
 public class CreateChallenge : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Visuals.SelectToggle();
     }
 
     // Update is called once per frame
